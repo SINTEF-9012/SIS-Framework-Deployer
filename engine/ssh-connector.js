456866e612c006310d65673da1173cff73c32dfd
nicolasferry <nicolas.ferry@sintef.no>
2017-12-18 16:38:37 +0100

    first version of the ssh connectore, just used to started node-red on android so far

---
+++ engine/ssh-connector.js
@@ -0,0 +1,39 @@
+var SSH = require('simple-ssh');
+var fs = require('fs');
+var emitter = require('events').EventEmitter;
+
+var ssh_connector = function () {
+
+    var that = new emitter();
+
+
+    that.initialize = function (ip, port, key) {
+        console.log("plpo");
+        fs.readFile(key, 'utf8', function (err, data) {
+            if (err) {
+                return console.log("::" + err);
+            }
+            console.log("Create ssh");
+            that.ssh = new SSH({
+                host: ip,
+                port: port,
+                key: data
+            });
+            that.emit("initialized");
+        });
+    };
+
+
+    that.executeCommand = function (command) {
+        that.ssh.exec(command, {
+            exit: function (code) {
+                console.log(code);
+            }
+        }).start();
+    };
+
+
+    return that;
+};
+
+module.exports = ssh_connector;
